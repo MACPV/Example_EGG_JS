b9252e14f5f84e3ef5a8e41c73de178e1f479650
macpv <aldo.pintado.98@gmail.com>
2024-04-18 20:46:00 -0500

    acrualizacion

---
+++ lengthOf.js
@@ -0,0 +1,10 @@
+
+function lengthOf(text) {
+    if (text == "") {
+        return "vacio";
+    } else {
+        return text.length;
+    }
+}
+console.log(lengthOf("hello"));
+console.log(lengthOf(""));
