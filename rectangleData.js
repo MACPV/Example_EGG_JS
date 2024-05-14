aab02d56e685a3fe89e0fd89e800a97ad61c4778
macpv <aldo.pintado.98@gmail.com>
2024-05-14 06:26:52 -0500

    update modules

--- rectangleData.js
+++ rectangleData.js
@@ -1,3 +1,18 @@
-export function DatosRectangulo(){
-    
+
+function Rectangulo(base, altura) {
+    this.base = +base;
+    this.altura = +altura;
 }
+
+
+
+
+const rectangulo1 = new Rectangulo(10, 8);
+const rectangulo2 = new Rectangulo(5, 4);
+const rectangulo3 = new Rectangulo(3, 6);
+const rectangulo4 = {
+    base: 10,
+    altura: 8
+}
+
+export const DatosRectangulo = [rectangulo1, rectangulo2, rectangulo3, rectangulo4];
